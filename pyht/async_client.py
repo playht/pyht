8b227792e87d3ad477f8c5ca55e264d68e80acca
Nicholas Carroll <nicholas.carroll@play.ht>
2023-10-10 21:46:51 -0700

    Splits client into client.py and async_client.py, enables input streaming.

--- pyht/async_client.py
+++ pyht/async_client.py
@@ -1,36 +1,42 @@
 from __future__ import annotations
+from typing import Any, AsyncGenerator, AsyncIterable, AsyncIterator, Callable, Coroutine, List, Tuple
 
-from typing import Generator, Iterable, Iterator, List, Tuple
-
+import asyncio
 from dataclasses import dataclass
 from datetime import datetime, timedelta
 import io
-import queue
-import re
-import threading
 
-from grpc import Channel, insecure_channel, secure_channel, ssl_channel_credentials
+from grpc.aio import Channel, insecure_channel, secure_channel
+from grpc import ssl_channel_credentials
 
+
+from .client import TTSOptions, SENTENCE_END_REGEX, SENTENCE_SPLIT_REGEX
 from .lease import Lease, LeaseFactory
 from .protos import api_pb2, api_pb2_grpc
-from .protos.api_pb2 import Format
 
 
-SENTENCE_END_REGEX = re.compile('.*[-.!?;:…]$')
-SENTENCE_SPLIT_REGEX = re.compile('\\w[-.!?;:…]\\W')
+class _Timer(object):
+    def __init__(self, timeout: float, callback: Callable[[], Coroutine[Any, Any, None]]):
+        self._timeout = timeout
+        self._callback = callback
+        self._task: asyncio.Task | None = None
+
+    async def _run(self):
+        await asyncio.sleep(self._timeout)
+        await self._callback()
+
+    def start(self):
+        if self._task is not None:
+            return
+        self._task = asyncio.ensure_future(self._run())
+
+    def cancel(self):
+        if self._task is None:
+            return
+        self._task.cancel()
 
 
-@dataclass
-class TTSOptions:
-    voice: str
-    format: Format = Format.FORMAT_WAV
-    sample_rate: int = 24000
-    quality: str = "faster"
-    temperature: float = 0.5
-    top_p: float = 0.5
-
-
-class Client(object):
+class AsyncClient(object):
     @dataclass
     class AdvancedOptions:
         api_url: str = "https://api.play.ht/api"
@@ -43,77 +49,77 @@ class Client(object):
         user_id: str,
         api_key: str,
         auto_connect: bool = True,
-        advanced: "Client.AdvancedOptions" | None = None,
+        advanced: "AsyncClient.AdvancedOptions" | None = None,
     ):
         assert user_id, "user_id is required"
         assert api_key, "api_key is required"
-
         self._advanced = advanced or self.AdvancedOptions()
-        
+
         self._lease_factory = LeaseFactory(user_id, api_key, self._advanced.api_url)
         self._lease: Lease | None = None
         self._rpc: Tuple[str, Channel] | None = None
-        self._lock = threading.Lock()
-        self._timer: threading.Timer | None = None
+        self._lock = asyncio.Lock()
+        self._timer: _Timer | None = None
         if auto_connect:
-            self.refresh_lease()
+            asyncio.ensure_future(self.refresh_lease())
 
-    def _schedule_refresh(self):
+    async def _schedule_refresh(self):
         assert self._lock.locked
         refresh_in = (
             self._lease.expires - timedelta(minutes=5) - datetime.now()
         ).total_seconds()
-        self._timer = threading.Timer(refresh_in, self.refresh_lease)
+        self._timer = _Timer(refresh_in, self.refresh_lease)
         self._timer.start()
 
-    def refresh_lease(self):
+    async def refresh_lease(self):
         """Manually refresh credentials with Play.ht."""
-        with self._lock:
-            if self._lease and self._lease.expires > datetime.now() + timedelta(minutes=5):
+        async with self._lock:
+            if self._lease and self._lease.expires > datetime.now() - timedelta(minutes=5):
                 if self._advanced.auto_refresh_lease and self._timer is None:
-                    self._schedule_refresh()
+                    await self._schedule_refresh()
                 return
             self._lease = self._lease_factory()
             grpc_addr = self._advanced.grpc_addr or self._lease.metadata["inference_address"]
             if self._rpc and self._rpc[0] != grpc_addr:
-                self._rpc[1].close()
+                await self._rpc[1].close()
                 self._rpc = None
-            if not self._rpc:
+            if self._rpc is None:
                 channel = (
-                    insecure_channel(grpc_addr)
-                    if self._advanced.insecure
+                    insecure_channel(grpc_addr) if self._advanced.insecure
                     else secure_channel(grpc_addr, ssl_channel_credentials())
                 )
                 self._rpc = (grpc_addr, channel)
-            if self._timer:
+            if self._timer is not None:
                 self._timer.cancel()
-            
-            if self._advanced.auto_refresh_lease:
-                self._schedule_refresh()
 
-    def stream_tts_input(
+            if self._advanced.auto_refresh_lease:
+                await self._schedule_refresh()
+
+    async def stream_tts_input(
         self,
-        text_stream: Generator[str, None, None] | Iterable[str],
+        text_stream: AsyncGenerator[str, None] | AsyncIterable[str],
         options: TTSOptions
-    ) -> Iterable[bytes]:
+    ):
         """Stream input to Play.ht via the text_stream object."""
         buffer = io.StringIO()
-        for text in text_stream:
+        async for text in text_stream:
             t = text.strip()
             buffer.write(t)
             buffer.write(" ")  # normalize word spacing.
             if SENTENCE_END_REGEX.match(t) is None:
                 continue
-            yield from self.tts(buffer.getvalue(), options)
+            async for data in self.tts(buffer.getvalue(), options):
+                yield data
             buffer = io.StringIO()
         # If text_stream closes, send all remaining text, regardless of sentence structure.
         if buffer.tell() > 0:
-            yield from self.tts(buffer.getvalue(), options)
+            async for data in self.tts(buffer.getvalue(), options):
+                yield data
 
-    def tts(self, text: str | List[str], options: TTSOptions) -> Iterable[bytes]:
-        self.refresh_lease()
-        with self._lock:
-            assert self._lease is not None and self._rpc is not None, "No connection"
+    async def tts(self, text: str | List[str], options: TTSOptions) -> AsyncIterable[bytes]:
+        await self.refresh_lease()
+        async with self._lock:
+            assert self._lease is not None and self._rpc is not None
             lease_data = self._lease.data
 
         if isinstance(text, str):
@@ -144,53 +150,58 @@ class Client(object):
         )
         request = api_pb2.TtsRequest(params=params, lease=lease_data)
         stub = api_pb2_grpc.TtsStub(self._rpc[1])
-        response = stub.Tts(request)  # type: Iterable[api_pb2.TtsResponse]
-        for item in response:
+        stream = stub.Tts(request)
+        async for item in stream:
             yield item.data
 
     def get_stream_pair(self, options: TTSOptions) -> Tuple['_InputStream', '_OutputStream']:
         """Get a linked pair of (input, output) streams.
         
-        These stream objects are thread-aware and safe to use in separate threads.
+        These stream objects ARE NOT thread-safe. Coroutines using these stream objects must
+        run on the same thread.
         """
-        shared_q = queue.Queue()
+        shared_q = asyncio.Queue()
         return (
             _InputStream(self, options, shared_q),
             _OutputStream(shared_q)
         )
 
-    def close(self):
-        if self._timer:
+    async def close(self):
+        if self._timer is not None:
             self._timer.cancel()
             self._timer = None
-        if self._rpc:
-            self._rpc[1].close()
+        if self._rpc is not None:
+            await self._rpc[1].close()
             self._rpc = None
 
     def __del__(self):
-        self.close()
+        try:
+            asyncio.get_running_loop()
+            asyncio.ensure_future(self.close())
+        except RuntimeError:
+            asyncio.run(self.close())
 
 
-class TextStream(Iterator[str]):
-    def __init__(self, q: queue.Queue | None = None):
+class TextStream(AsyncIterator[str]):
+    def __init__(self, q: asyncio.Queue[str | None] | None = None):
         super().__init__()
-        self._q = q or queue.Queue()
+        self._q = q or asyncio.Queue()
+        self._futures = []
 
-    def __iter__(self) -> Iterator[str]:
+    def __aiter__(self) -> AsyncIterator[str]:
         return self
 
-    def __next__(self) -> str:
-        value = self._q.get()
+    async def __anext__(self) -> str:
+        value = await self._q.get()
         if value is None:
-            raise StopIteration()
+            raise StopAsyncIteration()
         return value
     
-    def __call__(self, *args: str):
-        for a in args:
-            self._q.put(a)
+    async def __call__(self, *args: str):
+        await asyncio.gather(*(self._q.put(a) for a in args))
 
-    def close(self):
-        self._q.put(None)
+    async def close(self):
+        await self._q.put(None)
 
 
 class _InputStream(object):
@@ -201,28 +212,27 @@ class _InputStream(object):
        input_stream += 'Add another sentence to the stream.'
        input_stream.done()
     """
-    def __init__(self, client: Client, options: TTSOptions, q: queue.Queue[bytes|None]):
+    def __init__(self, client: AsyncClient, options: TTSOptions, q: asyncio.Queue[bytes|None]):
         self._input = TextStream()
-        def listen():
-            for output in client.stream_tts_input(self._input, options):
-                q.put(output)
-            q.put(None)
+        async def listen():
+            async for output in client.stream_tts_input(self._input, options):
+                await q.put(output)
+            await q.put(None)
 
-        self._listener = threading.Thread(None, listen, 'listen-thread')
-        self._listener.start()
+        self._listener = asyncio.ensure_future(listen())
 
-    def __call__(self, *args: str):
-        self._input(*args)
+    def __call__(self, *args: str) -> Coroutine[Any, Any, None]:
+        return self._input(*args)
 
-    def __iadd__(self, input: str):
-        self._input(input)
+    def __iadd__(self, input: str) -> Coroutine[Any, Any, None]:
+        return self._input(input)
 
-    def done(self):
-        self._input.close()
-        self._listener.join()
+    async def done(self):
+        await self._input.close()
+        await asyncio.wait_for(self._listener, 10)
 
 
-class _OutputStream(Iterator[bytes]):
+class _OutputStream(AsyncIterator[bytes]):
     """Iterator for output audio.
 
     usage:
@@ -230,25 +240,26 @@ class _OutputStream(Iterator[bytes]):
            <do stuff with audio bytes>
         output_stream.close()
     """
-    def __init__(self, q: queue.Queue[bytes|None]):
-        self._close = threading.Event()
+    def __init__(self, q: asyncio.Queue[bytes|None]):
+        self._close = asyncio.Event()
         self._q = q
 
-    def __iter__(self) -> Iterator[bytes]:
+    def __aiter__(self) -> AsyncIterator[bytes]:
         return self
 
-    def __next__(self) -> bytes:
+    async def __anext__(self) -> bytes:
         while True:
             try:
-                value = self._q.get(timeout = 0.05)
+                value = await asyncio.wait_for(self._q.get(), timeout = 0.05)
                 break
-            except queue.Empty as e:
+            except asyncio.TimeoutError as e:
                 if self._close.is_set():
-                    raise StopIteration() from e
+                    raise StopAsyncIteration() from e
                 continue
         if value is None:
-            raise StopIteration()
-        return value
+            raise StopAsyncIteration()
+        else:
+            return value
     
     def close(self):
         self._close.set()
