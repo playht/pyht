a8cd64039d43c5ed2426d7a168faaaaeb157cb98
Bryan Anderson <bryan@play.ht>
2024-10-09 23:09:48 +0000

    Update voice engine names and docs

--- demo/README.md
+++ demo/README.md
@@ -1,27 +1,26 @@
 # PyHT Streaming Client Demo
 
-## Prereqs
-This demo depends on the following packages:
-```
-numpy
-simpleaudio
-```
-
 ## Usage
 
 ```
 $ python demo/main.py --help
 
-usage: PyHT Streaming Demo [-h] --user USER --key KEY [--voice VOICE] [--quality {fast,faster}] (--text TEXT [TEXT ...] | --interactive)
+usage: PyHT Streaming Demo [-h] (--ws | --http | --grpc) [--async] --user USER --key KEY [--voice VOICE]
+                           [--language {afrikaans,albanian,amharic,arabic,bengali,bulgarian,catalan,croatian,czech,danish,dutch,english,french,galician,german,greek,hebrew,hindi,hungarian,indonesian,italian,japanese,korean,malay,mandarin,polish,portuguese,russian,serbian,spanish,swedish,tagalog,thai,turkish,ukrainian,urdu,xhosa}]
+                           (--text TEXT [TEXT ...] | --interactive)
 
 options:
   -h, --help            show this help message and exit
+  --ws                  Use the WebSocket API with the 3.0-mini model.
+  --http                Use the HTTP API with the 3.0-mini model.
+  --grpc                Use the gRPC API with the 2.0-turbo model.
+  --async               Use the asyncio client.
   --user USER, -u USER  Your Play.ht User ID.
   --key KEY, -k KEY     Your Play.ht API key.
   --voice VOICE, -v VOICE
                         Voice manifest URI
-  --quality {fast,faster}, -q {fast,faster}
-                        Quality of the generated audio
+  --language {afrikaans,albanian,amharic,arabic,bengali,bulgarian,catalan,croatian,czech,danish,dutch,english,french,galician,german,greek,hebrew,hindi,hungarian,indonesian,italian,japanese,korean,malay,mandarin,polish,portuguese,russian,serbian,spanish,swedish,tagalog,thai,turkish,ukrainian,urdu,xhosa}, -l {...}
+                        Language of the text to be spoken.
   --text TEXT [TEXT ...], -t TEXT [TEXT ...]
                         Text to generate, REQUIRED if the `--interactive` flag is not set.
   --interactive, -i     Run this demo in interactive-input mode, REQUIRED if `--text` is not supplied.
--- pyht/async_client.py
+++ pyht/async_client.py
@@ -65,7 +65,7 @@ class AsyncClient:
         auto_refresh_lease: bool = True
         disable_lease_disk_cache: bool = False
 
-        # HTTP/WebSocket (Play3.0)
+        # HTTP/WebSocket (Play3.0-mini-http, Play3.0-mini-ws)
         inference_coordinates_options: InferenceCoordinatesOptions = field(default_factory=InferenceCoordinatesOptions)
 
     def __init__(
@@ -254,9 +254,9 @@ class AsyncClient:
     ) -> AsyncIterable[bytes]:
         metrics = self._telemetry.start("tts-request")
         try:
-            if voice_engine is None or voice_engine == "Play3.0":
+            if voice_engine is None or voice_engine == "Play3.0-mini-http" or voice_engine == "Play3.0":
                 return self._tts_http(text, options, voice_engine, metrics)
-            elif voice_engine == "Play3.0-ws":
+            elif voice_engine == "Play3.0-mini-ws" or voice_engine == "Play3.0-ws":
                 return self._tts_ws(text, options, voice_engine, metrics)
             else:
                 return self._tts_grpc(text, options, voice_engine, metrics)
@@ -272,8 +272,8 @@ class AsyncClient:
         metrics: Metrics,
         context: AsyncContext | None = None
     ) -> AsyncIterable[bytes]:
-        if voice_engine == "Play3.0":
-            raise ValueError("Play3.0 is only supported in the HTTP API; otherwise use the gRPC API.")
+        if voice_engine.startswith("Play3.0"):
+            raise ValueError("Play3.0-* models are only supported in the HTTP and WebSocket APIs, not in the gRPC API.")
 
         start = time.perf_counter()
         await self.refresh_lease()
@@ -350,9 +350,11 @@ class AsyncClient:
         context: AsyncContext | None = None
     ) -> AsyncIterable[bytes]:
         if voice_engine is None:
-            voice_engine = "Play3.0"
-        if voice_engine != "Play3.0":
-            raise ValueError("Only Play3.0 is supported in the HTTP API; otherwise use the gRPC API.")
+            voice_engine = "Play3.0-mini-http"
+        if voice_engine == "Play3.0":
+            logging.warning("Voice engine Play3.0 is deprecated; use Play3.0-mini-http instead.")
+        elif voice_engine != "Play3.0-mini-http":
+            raise ValueError("Only Play3.0-mini-http is supported in the HTTP API; otherwise use the gRPC API.")
 
         start = time.perf_counter()
         await self.ensure_inference_coordinates()
@@ -413,9 +415,11 @@ class AsyncClient:
         context: AsyncContext | None = None
     ) -> AsyncIterable[bytes]:
         if voice_engine is None:
-            voice_engine = "Play3.0-ws"
-        if voice_engine != "Play3.0-ws":
-            raise ValueError("Only Play3.0-ws is supported in the WebSocket API")
+            voice_engine = "Play3.0-mini-ws"
+        if voice_engine == "Play3.0-ws":
+            logging.warning("Voice engine Play3.0-ws is deprecated; use Play3.0-mini-ws instead.")
+        elif voice_engine != "Play3.0-mini-ws":
+            raise ValueError("Only Play3.0-mini-ws is supported in the WebSocket API")
 
         start = time.perf_counter()
         await self.ensure_inference_coordinates()
